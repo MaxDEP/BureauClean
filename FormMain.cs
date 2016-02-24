41fb71b030620e2b4b281cf31c22b695b83ac47d
Ilhat Anthony <tallcraft@outlook.com>
2016-02-24 16:05:27 +0100

    Test de modification

---
+++ FormMain.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BureauClean_Projet
+{
+    public partial class frmNotifier : Form
+    {
+        private StringBuilder m_Sb;
+        private bool m_bDirty;test
+        private System.IO.FileSystemWatcher m_Watcher;
+        private bool m_bIsWatching;
+
+        public frmNotifier()
+        {
+            InitializeComponent();
+            m_Sb = new StringBuilder();
+            m_bDirty = false;
+            m_bIsWatching = false;
+        }
+
+        private void btnWatchFile_Click(object sender, EventArgs e)
+        {
+            if (m_bIsWatching)
+            {
+                m_bIsWatching = false;
+                m_Watcher.EnableRaisingEvents = false;
+                m_Watcher.Dispose();
+                btnWatchFile.BackColor = Color.LightSkyBlue;
+                btnWatchFile.Text = "Start Watching";
+                
+            }
+            else
+            {
+                m_bIsWatching = true;
+                btnWatchFile.BackColor = Color.Red;
+                btnWatchFile.Text = "Stop Watching";
+
+                m_Watcher = new System.IO.FileSystemWatcher();
+                m_Watcher.Filter = "*.*";
+                m_Watcher.Path = txtFile.Text + "\\";
+                if (chkSubFolder.Checked)
+
+                m_Watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                     | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                m_Watcher.Changed += new FileSystemEventHandler(OnChanged);
+                m_Watcher.Created += new FileSystemEventHandler(OnChanged);
+                m_Watcher.Deleted += new FileSystemEventHandler(OnChanged);
+                m_Watcher.Renamed += new RenamedEventHandler(OnRenamed);
+                m_Watcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (!m_bDirty)
+            {
+                m_Sb.Remove(0, m_Sb.Length);
+                m_Sb.Append(e.FullPath);
+                m_Sb.Append(" ");
+                m_Sb.Append(e.ChangeType.ToString());
+                m_Sb.Append("    ");
+                m_Sb.Append(DateTime.Now.ToString());
+                m_bDirty = true;
+            }
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            if (!m_bDirty)
+            {
+                m_Sb.Remove(0, m_Sb.Length);
+                m_Sb.Append(e.OldFullPath);
+                m_Sb.Append(" ");
+                m_Sb.Append(e.ChangeType.ToString());
+                m_Sb.Append(" ");
+                m_Sb.Append("to ");
+                m_Sb.Append(e.Name);
+                m_Sb.Append("    ");
+                m_Sb.Append(DateTime.Now.ToString());
+                m_bDirty = true;
+            }            
+        }
+
+        private void tmrEditNotify_Tick(object sender, EventArgs e)
+        {
+            if (m_bDirty)
+            {
+                lstNotification.BeginUpdate();
+                lstNotification.Items.Add(m_Sb.ToString());
+                lstNotification.EndUpdate();
+                m_bDirty = false;
+            }
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            if (rdbDir.Checked)
+            {
+                DialogResult resDialog = dlgOpenDir.ShowDialog();
+                if (resDialog.ToString() == "OK")
+                {
+                    txtFile.Text = dlgOpenDir.SelectedPath;
+                }
+            }
+            else
+            {
+                DialogResult resDialog = dlgOpenFile.ShowDialog();
+                if (resDialog.ToString() == "OK")
+                {
+                    txtFile.Text = dlgOpenFile.FileName;
+                }
+            }
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            DialogResult resDialog = dlgSaveFile.ShowDialog();
+            if (resDialog.ToString() == "OK")
+            {
+                FileInfo fi = new FileInfo(dlgSaveFile.FileName);
+                StreamWriter sw = fi.CreateText();
+                foreach (string sItem in lstNotification.Items)
+                {
+                    sw.WriteLine(sItem);
+                }
+                sw.Close();
+            }
+        }
+
+        private void rdbDir_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbDir.Checked == true)
+            {
+                chkSubFolder.Enabled = true;
+            }
+        }
+
+        private void frmNotifier_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
