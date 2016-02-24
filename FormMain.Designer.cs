178feee9a0ad4e0a7bb4e5b79396ee4f99c77fa5
Ilhat Anthony <tallcraft@outlook.com>
2016-02-24 16:32:19 +0100

    Delete input url folder

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -29,8 +29,6 @@ namespace BureauClean_Projet
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.txtFile = new System.Windows.Forms.TextBox();
-            this.lblFile = new System.Windows.Forms.Label();
             this.btnWatchFile = new System.Windows.Forms.Button();
             this.lstNotification = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -40,29 +38,12 @@ namespace BureauClean_Projet
             this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
-            // txtFile
-            // 
-            this.txtFile.Location = new System.Drawing.Point(13, 32);
-            this.txtFile.Name = "txtFile";
-            this.txtFile.Size = new System.Drawing.Size(268, 20);
-            this.txtFile.TabIndex = 0;
-            // 
-            // lblFile
-            // 
-            this.lblFile.AutoSize = true;
-            this.lblFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFile.Location = new System.Drawing.Point(12, 14);
-            this.lblFile.Name = "lblFile";
-            this.lblFile.Size = new System.Drawing.Size(124, 13);
-            this.lblFile.TabIndex = 1;
-            this.lblFile.Text = "Select File/Directory";
-            // 
             // btnWatchFile
             // 
             this.btnWatchFile.BackColor = System.Drawing.Color.LightSkyBlue;
             this.btnWatchFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnWatchFile.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnWatchFile.Location = new System.Drawing.Point(12, 58);
+            this.btnWatchFile.Location = new System.Drawing.Point(12, 12);
             this.btnWatchFile.Name = "btnWatchFile";
             this.btnWatchFile.Size = new System.Drawing.Size(119, 23);
             this.btnWatchFile.TabIndex = 4;
@@ -73,7 +54,7 @@ namespace BureauClean_Projet
             // lstNotification
             // 
             this.lstNotification.FormattingEnabled = true;
-            this.lstNotification.Location = new System.Drawing.Point(12, 109);
+            this.lstNotification.Location = new System.Drawing.Point(12, 63);
             this.lstNotification.Name = "lstNotification";
             this.lstNotification.Size = new System.Drawing.Size(461, 225);
             this.lstNotification.TabIndex = 5;
@@ -82,7 +63,7 @@ namespace BureauClean_Projet
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 93);
+            this.label3.Location = new System.Drawing.Point(12, 47);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(125, 13);
             this.label3.TabIndex = 6;
@@ -109,8 +90,6 @@ namespace BureauClean_Projet
             this.ClientSize = new System.Drawing.Size(484, 457);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lstNotification);
-            this.Controls.Add(this.lblFile);
-            this.Controls.Add(this.txtFile);
             this.Controls.Add(this.btnWatchFile);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -123,9 +102,6 @@ namespace BureauClean_Projet
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtFile;
-        private System.Windows.Forms.Label lblFile;
         private System.Windows.Forms.Button btnWatchFile;
         private System.Windows.Forms.ListBox lstNotification;
         private System.Windows.Forms.Label label3;
