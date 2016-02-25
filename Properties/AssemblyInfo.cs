4c00c0545c8fb16be964e161db69ecebf33224f9
Ilhat Anthony <tallcraft@outlook.com>
2016-02-25 15:51:23 +0100

    Update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.9")]
-[assembly: AssemblyFileVersion("1.0.0.9")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: NeutralResourcesLanguage("fr")]
 
